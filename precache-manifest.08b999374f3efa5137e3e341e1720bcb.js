98ea436058215588438e5cf4e62938b5dc1d4674
Muhammad Owais <owais.md29@gmail.com>
2020-10-25 19:22:32 +0500

    Updates

---
+++ precache-manifest.08b999374f3efa5137e3e341e1720bcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36332a698396e0dffa8b29ce19cfcc0f",
+    "url": "/React-Application-Owais/index.html"
+  },
+  {
+    "revision": "fbec237a642a3bc4acf7",
+    "url": "/React-Application-Owais/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3138bd5a555633819e31",
+    "url": "/React-Application-Owais/static/css/main.49cdc254.chunk.css"
+  },
+  {
+    "revision": "fbec237a642a3bc4acf7",
+    "url": "/React-Application-Owais/static/js/2.19afb97a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Application-Owais/static/js/2.19afb97a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3138bd5a555633819e31",
+    "url": "/React-Application-Owais/static/js/main.29532d02.chunk.js"
+  },
+  {
+    "revision": "d512335a920e1dddf661",
+    "url": "/React-Application-Owais/static/js/runtime-main.1d041c45.js"
+  }
+]);
